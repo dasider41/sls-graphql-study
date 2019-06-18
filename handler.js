55673d6f73836b46828a9f8879cc53dd24226c06
nichibo-junwoo <junwoo@nichibojapan.com>
2019-06-19 10:30:25 +1200

    Rename handler and path

--- handler.js
+++ handler.js
@@ -2,4 +2,4 @@ import { ApolloServer } from "apollo-server-lambda";
 import { typeDefs, resolvers } from "./schema";
 
 const server = new ApolloServer({ typeDefs, resolvers });
-export const query = server.createHandler();
+export const graphql = server.createHandler();
