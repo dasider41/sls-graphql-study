b9f3a71551e7bc77ca3b4194a3ae5cec2fc99358
nichibo-junwoo <junwoo@nichibojapan.com>
2019-06-18 12:19:16 +1200

    Modify handler with sugar syntax

--- handler.js
+++ handler.js
@@ -1,8 +1,7 @@
 import { graphql } from "graphql";
 import { schema, resolvers } from "./schema";
 
-export const query = (event, context, callback) => {
-  graphql(schema, event.body, resolvers)
-    .then(res => callback(null, { statusCode: 200, body: JSON.stringify(res) }))
-    .catch(callback);
+export const query = async event => {
+  const res = await graphql(schema, event.body, resolvers);
+  return { statusCode: 200, body: JSON.stringify(res) };
 };
